06554064bbdf7811a52cea9a1793c81debf2f35f
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-08-25 00:29:45 -0700

    one more version fix.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=['berlioz', 'berlioz/frameworks'],
     include_package_data=True,
     long_description=read('README.rst'),
-    python_requires='>=2.7, >=3.6',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'boto3>=1.7.41',
         'requests>=2.19.1',
